86a709bd425c588bf051d0a1ea63e5c005bba694
dream0hyeah <47338460+dream0hyeah@users.noreply.github.com>
2019-02-21 15:51:17 -0500

    ProgrammingAssignment2-master
    I installed Git on my computer, but I couldn' t find it.

---
+++ cachematrix.R
@@ -0,0 +1,35 @@
+## Put comments here that give an overall description of what your
+## functions do
+## Create a special object that stores a numeric matrix and cache's its inverse.
+## Write a short comment describing this function
+## Set the value of the matrix,get the value of the matrix,set the value of the inverse,get the value of the inverse
+makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+
+## Write a short comment describing this function
+## Calculate the inverse of the special "matrix" created with the above function
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if (!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
+  }
